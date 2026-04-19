c88f23901fe154e4b39de25f97e1d434b6cec194
jhahimanshu04 <hjha63521@gmail.com>
2026-04-19 16:43:48 +0530

    interview api

--- Frontend/src/interview/services/interviewapi.js
+++ Frontend/src/interview/services/interviewapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"https://prepai-hliu.onrender.com"
+  baseURL:"https://prepai-hliu.onrender.com",
   withCredentials: true,
 });
 
