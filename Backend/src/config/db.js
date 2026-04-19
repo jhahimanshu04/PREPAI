04b95399b78075ec3c01eaff564939ebe9e87e17
jhahimanshu04 <hjha63521@gmail.com>
2026-04-19 14:21:14 +0530

    changes

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -38,6 +38,12 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
+     dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
+    }
   }
 );
 
