e2cad19b4bc70bc58482d3cb0c2f4f83189d2dd8
jhahimanshu04 <hjha63521@gmail.com>
2026-04-19 15:35:47 +0530

    frontend url

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 app.use("/api/auth", userRoutes);
 app.use("/api/interview", interviewRoutes);
 
